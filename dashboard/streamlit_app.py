e4e67ec07ecf71c71b765638c1e29c85101faa96
Your Name <your@email.com>
2026-08-12 04:45:58 +0500

    Paksitan uk emoji

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 from datetime import datetime, timedelta
 
-st.set_page_config(page_title="Pearls AQI Predictor", page_icon="🌍", layout="wide")
+st.set_page_config(page_title="Pearls AQI Predictor 🇵🇰,🇬🇧", page_icon="🌍", layout="wide")
 st.markdown("""
 <style>
 .block-container {
